b47a987f0b1fdac1d6d9b8f17a8e2b7de3afb4f8
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-12-24 09:35:17 +0100

    Petrinet execution semantics

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # micropm4py
 Process Mining in resource-constrained environments
+
+Scripts tested on Cortex-M3 with 64KB of RAM
---
+++ petrinet_exec.py
@@ -0,0 +1,30 @@
+def exec(p, m, a):
+    for t in p[1]:
+        if t[0] == a:
+            o = True
+            for p, v in t[1].items():
+                if p not in m or v < m[p]:
+                    o = False
+                    break
+            if o:
+                for p, v in t[1].items():
+                    m[p] = m[p] - v
+                    if m[p] == 0:
+                        del m[p]
+                for p, v in t[2].items():
+                    if p not in m:
+                        m[p] = 0
+                    m[p] = m[p] + v
+                return m
+
+def ex_trace(p, m, fm, tr):
+    for act in tr:
+        m = exec(p, m, act)
+        if m is None:
+            return False
+    return m == fm
+
+if __name__ == "__main__":
+    p = [["source", "p1", "sink"], [["A", {0: 1}, {1: 1}], ["B", {1: 1}, {2: 1}]]]
+    r = ex_trace(p, {0: 1}, {2: 1}, ["A", "B"])
+    print(r)
