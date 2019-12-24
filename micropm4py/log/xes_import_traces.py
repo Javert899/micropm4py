7795826ce2bdb0a54e89e0724e225e3ac3e90148
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-12-24 16:37:40 +0100

    DFG calculation when importing XES

--- micropm4py/log/xes_import_traces.py
+++ micropm4py/log/xes_import_traces.py
@@ -19,10 +19,29 @@ def r(tr, p, on, row):
             on = 1
     return tr, p, on
 
+def udf(dfg, tr):
+    if len(tr) > 0:
+        i = 0
+        while i < len(tr):
+            if not tr[i] in dfg[0]:
+                dfg[0].append(tr[i])
+                dfg[1][len(dfg[0])-1] = 0
+            dfg[1][dfg[0].index(tr[i])] = dfg[1][dfg[0].index(tr[i])] + 1
+            if i > 0:
+                tup = (dfg[0].index(tr[i - 1]), dfg[0].index(tr[i]))
+                if tup not in dfg[4]:
+                    dfg[4][tup] = 0
+                dfg[4][tup] = dfg[4][tup] + 1
+            i = i + 1
+        dfg[2].add(dfg[0].index(tr[0]))
+        dfg[3].add(dfg[0].index(tr[-1]))
+    return dfg
+
 if __name__ == "__main__":
     tr = None
     p = None
     on = 0
+    dfg = [[], dict(), set(), set(), dict()]
     tr, p, on = r(tr, p, on, "<?xml version='1.0' encoding='UTF-8'?>")
     tr, p, on = r(tr, p, on, "<log>")
     tr, p, on = r(tr, p, on, "\t<trace>")
@@ -37,6 +56,7 @@ if __name__ == "__main__":
     tr, p, on = r(tr, p, on, "\t\t</event>")
     tr, p, on = r(tr, p, on, "\t</trace>")
     print(tr)
+    dfg = udf(dfg, tr)
     tr, p, on = r(tr, p, on, "\t<trace>")
     tr, p, on = r(tr, p, on, "\t\t<event>")
     tr, p, on = r(tr, p, on, "\t\t\t<string key=\"concept:name\" value=\"A\" />")
@@ -46,4 +66,6 @@ if __name__ == "__main__":
     tr, p, on = r(tr, p, on, "\t\t</event>")
     tr, p, on = r(tr, p, on, "\t</trace>")
     print(tr)
+    dfg = udf(dfg, tr)
     tr, p, on = r(tr, p, on, "</log>")
+    print(dfg)
--- micropm4py/log/xes_import_traces_file.py
+++ micropm4py/log/xes_import_traces_file.py
@@ -1,6 +1,6 @@
 from micropm4py.log import xes_import_traces
 
-def imp_file(file_path):
+def imp_list_traces_from_file(file_path):
     list_traces = []
     tr = None
     p = None
@@ -14,3 +14,19 @@ def imp_file(file_path):
         line = F.readline()
     F.close()
     return list_traces
+
+def imp_dfg_file(file_path):
+    dfg = [[], dict(), set(), set(), dict()]
+    tr = None
+    p = None
+    on = 0
+    F = open(file_path, "r")
+    line = F.readline()
+    while line:
+        tr, p, on = xes_import_traces.r(tr, p, on, line)
+        if tr is not None:
+            dfg = xes_import_traces.udf(dfg, tr)
+        line = F.readline()
+    F.close()
+    return dfg
+
