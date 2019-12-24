9a6702a6c79a62b77011c3f2fcb49d9df832ba5e
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-12-24 15:00:57 +0100

    Moved files

---
+++ micropm4py/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.0.1'
+__doc__ = "MicroPM4Py - Process Mining for Micro-Controllers"
+__author__ = 'PADS'
+__author_email__ = 'a.berti@pads.rwth-aachen.de'
+__maintainer__ = 'PADS'
+__maintainer_email__ = "a.berti@pads.rwth-aachen.de"
---
+++ setup.py
@@ -0,0 +1,32 @@
+from os.path import dirname, join
+
+from setuptools import setup
+
+import micropm4py
+
+
+def read_file(filename):
+    with open(join(dirname(__file__), filename)) as f:
+        return f.read()
+
+
+setup(
+    name=micropm4py.__name__,
+    version=micropm4py.__version__,
+    description=micropm4py.__doc__.strip(),
+    long_description=read_file('README.md'),
+    author=micropm4py.__author__,
+    author_email=micropm4py.__author_email__,
+    py_modules=[micropm4py.__name__],
+    include_package_data=True,
+    packages=['micropm4py', 'micropm4py.log', 'micropm4py.petrinet'],
+    url='http://www.pm4py.org',
+    license='GPL 3.0',
+    install_requires=[
+    ],
+    project_urls={
+        'Documentation': 'http://pm4py.pads.rwth-aachen.de/documentation/',
+        'Source': 'https://github.com/pm4py/pm4py-source',
+        'Tracker': 'https://github.com/pm4py/pm4py-source/issues',
+    }
+)
