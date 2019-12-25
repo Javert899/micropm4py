72d2f8d7cf53d12b14a32287b8b1d7d6b6c1dd17
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-12-25 08:35:30 +0100

    Fixed tests

--- micro_tests/execute_tests.py
+++ micro_tests/execute_tests.py
@@ -1,9 +1,16 @@
 import unittest
-from micro_tests.test_petri import TestPetri
-from micro_tests.test_log import TestLog
-from micro_tests.test_conv import TestConv
+import inspect
+import os
+import sys
 
 if __name__ == "__main__":
+    current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    parent_dir = os.path.dirname(current_dir)
+    sys.path.insert(0, parent_dir)
+    from micro_tests.test_petri import TestPetri
+    from micro_tests.test_log import TestLog
+    from micro_tests.test_conv import TestConv
     t1 = TestPetri()
     t2 = TestLog()
+    t3 = TestConv()
     unittest.main()
