63d6d824a4c5126c31a2a0e9bce186b75201a091
Alessandro Berti <javert@northwood.northwood.net>
2019-12-30 09:34:17 +0100

    New method for importing

--- micropm4py/log/xes_import_traces_file.py
+++ micropm4py/log/xes_import_traces_file.py
@@ -1,6 +1,35 @@
 from micropm4py.log import xes_import_traces
 
 
+def get_line(F):
+    by = F.read(1)
+    line = ""
+    while by:
+        if by == "\r" or by == "\n":
+            by = F.read(1)
+            continue
+        line = line + by
+        if by == ">":
+            return line
+        by = F.read(1)
+
+
+def imp_log_certification(file_path):
+    list_traces = []
+    tr = None
+    p = None
+    on = 0
+    F = open(file_path, "r")
+    line = get_line(F)
+    while line:
+        tr, p, on = xes_import_traces.r(tr, p, on, line)
+        if tr is not None:
+            list_traces.append(tr)
+        line = get_line(F)
+    F.close()
+    return list_traces
+
+
 def imp_list_traces_from_file(file_path):
     list_traces = []
     tr = None
