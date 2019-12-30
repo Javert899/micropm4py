63e5e12bb75096cf5847ecd7d8878c92f3b2f3d9
Alessandro Berti <javert@northwood.northwood.net>
2019-12-30 13:27:16 +0100

    Increased version number

--- micropm4py/__init__.py
+++ micropm4py/__init__.py
@@ -1,6 +1,6 @@
 from micropm4py import log, petrinet, util, conversion, visualization
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 __doc__ = "MicroPM4Py - Process Mining for Micro-Controllers"
 __author__ = 'PADS'
 __author_email__ = 'a.berti@pads.rwth-aachen.de'
