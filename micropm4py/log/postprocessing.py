2a7803060abde60aef854cca19b5c21464bb77ec
Alessandro Berti <javert@northwood.northwood.net>
2020-01-01 10:32:10 +0100

    Bug fix

--- micropm4py/log/postprocessing.py
+++ micropm4py/log/postprocessing.py
@@ -2,7 +2,9 @@ def add_art_st_end(log):
     i = 0
     while i < len(log):
         if len(log[i]) == 2 and type(log[i][1]) is tuple:
+            log[i] = list(log[i])
             log[i][1] = [">>"] + list(log[i][1]) + ["[]"]
             log[i][1] = tuple(log[i][1])
+            log[i] = tuple(log[i])
         i = i + 1
     return log
