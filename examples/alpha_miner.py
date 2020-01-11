7d6a20a9f60b1628e678d583978797785f1c1493
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-01-11 17:56:00 +0100

    Revised example

--- examples/alpha_miner.py
+++ examples/alpha_miner.py
@@ -7,22 +7,27 @@ import time
 
 
 def main():
-    aa = time.time()
+    aa = time.ticks_ms()
     log_path = "../micro_tests/running-example.xes"
     dfg = xes_import_traces_file.imp_dfg_file(log_path)
-    bb = time.time()
+    bb = time.ticks_ms()
     print(bb-aa, "imported DFG")
     net, im, fm = alpha.alpha(dfg)
+    cc = time.ticks_ms()
+    print(cc-bb, "applied Alpha Miner")
     del dfg
     petri_print.print_dot(net)
+    dd = time.ticks_ms()
+    print(dd-cc, "performed visualization")
     it = xes_import_traces_file.get_it_from_file(log_path)
     nxt = xes_import_traces_file.get_nxt_trace(it)
     while nxt:
         print(petrinet_exec.ex_trace(net, copy.copy(im), copy.copy(fm), nxt[1]), nxt[1])
         nxt = xes_import_traces_file.get_nxt_trace(it)
-    cc = time.time()
-    print(cc-bb)
+    ee = time.ticks_ms()
+    print(ee-dd, "performed replay")
 
 
 if __name__ == "__main__":
+    time.ticks_ms = time.time
     main()
