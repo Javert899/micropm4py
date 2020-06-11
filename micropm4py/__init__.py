8f0c38820d88c4d2cdea41245d276615893d9ea2
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-06-11 09:57:05 +0200

    new version

--- micropm4py/__init__.py
+++ micropm4py/__init__.py
@@ -1,6 +1,6 @@
 from micropm4py import log, petrinet, util, conversion, visualization, random, discovery
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __doc__ = "MicroPM4Py - Process Mining for Micro-Controllers"
 __author__ = 'Alessandro Berti'
 __author_email__ = 'alessandro.berti89@gmail.com'
