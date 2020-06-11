048cee161d89ccef3502280f420f7f415e2df410
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-06-11 11:18:04 +0200

    revised example
    revised example

--- examples/alignments.py
+++ examples/alignments.py
@@ -9,12 +9,16 @@ def main():
     log_file = os.path.join("..", "micro_tests", "reviewing.xes")
     dfg = xes_import_traces_file.imp_dfg_file(log_file)
     net, im, fm = alpha.alpha(dfg)
+    del dfg
     it = xes_import_traces_file.get_it_from_file(log_file)
     nxt = xes_import_traces_file.get_nxt_trace(it)
     aa = time.ticks_ms()
     while nxt:
-        align = alignments.apply(nxt, net, im, fm, ret_tuple_as_trans_desc=False)
-        print(align)
+        try:
+            align = alignments.apply(nxt, net, im, fm, ret_tuple_as_trans_desc=False)
+            print(align)
+        except:
+            print("MemoryError")
         nxt = xes_import_traces_file.get_nxt_trace(it)
     bb = time.ticks_ms()
     print(bb - aa)
