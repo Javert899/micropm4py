4301e4bb20b3e9b81225da3d7f3a063353d772af
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-06-11 12:24:44 +0200

    importing/exporting DFG

--- micropm4py/log/dfg_import_export.py
+++ micropm4py/log/dfg_import_export.py
@@ -30,3 +30,33 @@ def import_dfg(file_path):
     dfg[3] = tuple(dfg[3])
     dfg = tuple(dfg)
     return dfg
+
+
+def print_dfg(dfg):
+    print("%d" % (len(dfg[0])))
+    for a in dfg[0]:
+        print("%s" % (a))
+    print("%d" % len(dfg[2]))
+    for a in dfg[2]:
+        print("%dx1" % (a))
+    print("%d" % (len(dfg[3])))
+    for a in dfg[3]:
+        print("%dx1" % (a))
+    for (a1, a2) in dfg[4]:
+        print("%d>%dx%d" % (a1, a2, dfg[4][(a1, a2)]))
+
+
+def export_dfg_file(dfg, file_path):
+    F = open(file_path, "w")
+    F.write("%d\n" % (len(dfg[0])))
+    for a in dfg[0]:
+        F.write("%s\n" % (a))
+    F.write("%d\n" % len(dfg[2]))
+    for a in dfg[2]:
+        F.write("%dx1\n" % (a))
+    F.write("%d\n" % (len(dfg[3])))
+    for a in dfg[3]:
+        F.write("%dx1\n" % (a))
+    for (a1, a2) in dfg[4]:
+        F.write("%d>%dx%d\n" % (a1, a2, dfg[4][(a1, a2)]))
+    F.close()
